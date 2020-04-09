6058580ad6058c69ef0296946f2521909b082279
xdc <1434412784@qq.com>
2020-04-09 23:15:34 +0800

    deploy

---
+++ precache-manifest.ea28ccb3309a19607335eca9c7eff911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434112de62518d319c04",
+    "url": "/morney-website/css/app.abeab6d7.css"
+  },
+  {
+    "revision": "9a53e4a7e92e631673779a90e980a6c4",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "434112de62518d319c04",
+    "url": "/morney-website/js/app.bf9459c9.js"
+  },
+  {
+    "revision": "3019074985fc2ef517ac",
+    "url": "/morney-website/js/chunk-vendors.783a441f.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
